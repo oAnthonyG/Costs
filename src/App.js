4275b87e8c5456cc385054eaea3d9f3658ef0bd7
anthony <anthonytguilherme@hotmail.com>
2023-08-14 16:45:44 -0300

    adding routes and some style shortcuts

--- src/App.js
+++ src/App.js
@@ -1,11 +1,34 @@
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import React from 'react';
+
+import Home from './components/pages/Home';
+import Company from './components/pages/Company';
+import Contact from './components/pages/Contact';
+import NewProject from './components/pages/NewProject';
+
+import Container from './components/layouts/Container';
+
+
+
 function App() {
   return (
-    <div >
-      <header>
-        <p>Cost</p>
-      </header>
-    </div>
+    <Router>
+      <ul>
+        <Link to='/'>Home</Link>
+        <Link to='/company'>Empresa</Link>
+        <Link to='/contact'>Contato</Link>
+        <Link to='/newproject'>Novo Projeto</Link>
+      </ul>
+      <Container customClass='min-height'>
+        <Routes>
+          <Route exact path='/' element={<Home />} />
+          <Route exact path='/company' element={<Company />} />
+          <Route exact path='/contact' element={<Contact />} />
+          <Route exact path='/newproject' element={<NewProject />} />
+        </Routes>
+      </Container>
+      <footer>Footer</footer>
+    </Router>
   );
 }
 
