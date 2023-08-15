ee474d1b89be3135ba76948b49b288efd6f49c5f
anthony <anthonytguilherme@hotmail.com>
2023-08-15 15:06:48 -0300

    created a linkbutton component and some stylish

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,5 +1,18 @@
-function Home(){
-  return <h1>home</h1>
+import LinkButton from '../layouts/LinkButton';
+
+import styles from './Home.module.css';
+import savings from '../../img/savings.svg';
+
+
+function Home() {
+  return (
+    <section className={styles.home_container}>
+      <h1>Bem-Vindo ao <span>Costs</span></h1>
+      <p>Comece a gerenciar os seus projetos agora mesmo!</p>
+      <LinkButton to="/newproject" text="Criar Projeto" />
+      <img src={savings} alt="Costs" />
+    </section>
+  )
 }
 
 export default Home;
