71d0f4b5d530437bdc026734b4f701829c6f62a6
anthony <anthonytguilherme@hotmail.com>
2023-08-15 17:13:20 -0300

    select and submit button componentized

--- src/components/form/Select.js
+++ src/components/form/Select.js
@@ -1,18 +1,13 @@
 import styles from './Select.module.css';
 
 
-function Select({ type, text, name, placeholder, handleOnChange, value }) {
+function Select({ text, name, options, handleOnChange, value }) {
   return (
     <div className={styles.form_control}>
       <label htmlFor={name}>{text}:</label>
-      <input
-        type={type}
-        name={name}
-        id={name}
-        placeholder={placeholder}
-        onChange={handleOnChange}
-        value={value}
-      />
+      <select name={name} id={name} >
+        <option>Selecione uma opção</option>
+      </select>
     </div>
   )
 }
