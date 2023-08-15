7d255244d85d1301513916057556621f645316ce
anthony <anthonytguilherme@hotmail.com>
2023-08-15 16:14:03 -0300

    refactoring inputs and starting with Select

---
+++ src/components/project/ProjectForm.js
@@ -0,0 +1,36 @@
+import Input from '../form/Input';
+
+import styles from './ProjectForm.module.css';
+
+function ProjectForm() {
+  return (
+    <form className={styles.form}>
+      <Input 
+      type="text" 
+      text="Nome do projeto" 
+      name="name" 
+      placeholder="Insira o nome do projeto"
+
+      />
+      <Input 
+      type="number" 
+      text="Orçamento do projeto" 
+      name="budget" 
+      placeholder="Insira o orçamento total"
+       
+      />
+      <div>
+        <select name="category_id">
+          <option disabled selected>
+            Selecione a categoria
+          </option>
+        </select>
+      </div>
+      <div>
+        <input type="submit" value="Criar projeto" />
+      </div>
+    </form>
+  )
+}
+
+export default ProjectForm;
