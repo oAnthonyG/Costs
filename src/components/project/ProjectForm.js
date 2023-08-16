db1a90a85b8fb5abc54cbbdabfab5ef28510bfd8
anthony <anthonytguilherme@hotmail.com>
2023-08-16 16:29:16 -0300

    Inserting data into the database via API

--- src/components/project/ProjectForm.js
+++ src/components/project/ProjectForm.js
@@ -6,8 +6,9 @@ import SubmitButton from '../form/SubmitButton';
 
 import styles from './ProjectForm.module.css';
 
-function ProjectForm({ btnText }) {
+function ProjectForm({ btnText, handleSubmit, projectData }) {
   const [categories, setCategories] = useState([]);
+  const [project, setProject] = useState(projectData || {})
 
 
   useEffect(() => {
@@ -15,7 +16,7 @@ function ProjectForm({ btnText }) {
       {
         method: "GET",
         headers: {
-          'Content-Type': 'application/json'
+          "content-type": "application/json"
         }
       })
       .then((resp) => resp.json())
@@ -25,28 +26,49 @@ function ProjectForm({ btnText }) {
       .catch((err) => console.log(err))
   }, []);
 
+  const submit = (e) => {
+    e.preventDefault()
+    handleSubmit(project)
 
+  }
+
+  function handleChange(e) {
+    setProject({ ...project, [e.target.name]: e.target.value })
+  }
+  function handleCategory(e) {
+    setProject({
+      ...project, category: {
+        id: e.target.value,
+        name: e.target.options[e.target.selectedIndex].text,
+
+      }
+    })
+  }
 
   return (
-    <form className={styles.form}>
+    <form onSubmit={submit} className={styles.form}>
       <Input
         type="text"
         text="Nome do projeto"
         name="name"
         placeholder="Insira o nome do projeto"
-
+        handleOnChange={handleChange}
+        value={project.name ? project.name : ''}
       />
       <Input
         type="number"
         text="Orçamento do projeto"
         name="budget"
         placeholder="Insira o orçamento total"
-
+        handleOnChange={handleChange}
+        value={project.budget ? project.budget : ''}
       />
       <Select
         name="category_id"
         text="Selecione a categoria"
         options={categories}
+        handleOnChange={handleCategory}
+        value={project.category ? project.category.id : ''}
       />
       <SubmitButton text={btnText} />
     </form>
