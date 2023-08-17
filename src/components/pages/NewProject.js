fe715c3323e481e573a9ab9d5dc93eed7b32437c
anthony <anthonytguilherme@hotmail.com>
2023-08-17 14:54:29 -0300

    created a success message of system

--- src/components/pages/NewProject.js
+++ src/components/pages/NewProject.js
@@ -20,7 +20,7 @@ function NewProject() {
     })
       .then((resp) => resp.json())
       .then((data) => {
-        history('/projects', { message: "Projeto criado com sucesso!" });
+        history('/projects', { state: { message: "Projeto criado com sucesso!" } });
       })
       .catch((err) => console.log(err))
   }
