19b52904f010bb9e7a17665fe3f0ba48006ece99
anthony <anthonytguilherme@hotmail.com>
2023-08-21 16:57:21 -0300

    created a function for the details of the project

--- src/components/pages/Project.js
+++ src/components/pages/Project.js
@@ -3,30 +3,64 @@ import styles from './Project.module.css';
 import { useEffect, useState } from 'react';
 
 import Loading from '../layouts/Loading';
+import Container from '../layouts/Container';
 
 function Project() {
   const { id } = useParams();
   const [project, setProject] = useState([]);
+  const [showProjectForm, setShowProjectForm] = useState(false);
 
   useEffect(() => {
-    setTimeout(() =>{
+    setTimeout(() => {
       fetch(`http://localhost:5000/projects/${id}`, {
-      method: 'GET',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-    })
-      .then((resp) => resp.json())
-      .then((data) => {
-        setProject(data)
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json'
+        },
       })
-      .catch((err) => console.log(err))
+        .then((resp) => resp.json())
+        .then((data) => {
+          setProject(data)
+        })
+        .catch((err) => console.log(err))
     }, 500)
   }, [id])
 
+  function toggleProjectForm() {
+    setShowProjectForm(!showProjectForm);
+  }
+
   return (
     <>
-      {project.name ? (<p>{project.name}</p>) : (
+      {project.name ? (
+        <div className={styles.project_details}>
+          <Container customClass="column">
+            <div className={styles.details_container}>
+              <h1>Projeto: {project.name}</h1>
+              <button className={styles.btn} onClick={toggleProjectForm}>
+                {!showProjectForm ? 'Editar projeto' : 'Fechar'}
+              </button>
+              {!showProjectForm ? (
+                <div className={styles.project_info}>
+                  <p>
+                    <span>Categoria:</span> {project.category.name}
+                  </p>
+                  <p>
+                    <span>Total de Orlamento:</span> R${project.budget}
+                  </p>
+                  <p>
+                    <span>Total Utilizado:</span> R${project.cost}
+                  </p>
+                </div>
+              ) : (
+                <div>
+                  <p>Detalhes do projeto</p>
+                </div>
+              )}
+            </div>
+          </Container>
+        </div>
+      ) : (
         <Loading />
       )}
     </>
