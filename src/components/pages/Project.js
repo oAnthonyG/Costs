85c83520753608f51c68afe65f34953990f28812
anthony <anthonytguilherme@hotmail.com>
2023-08-21 16:32:54 -0300

    a simple loading component in the project loading

--- src/components/pages/Project.js
+++ src/components/pages/Project.js
@@ -2,25 +2,35 @@ import { useParams } from 'react-router-dom';
 import styles from './Project.module.css';
 import { useEffect, useState } from 'react';
 
+import Loading from '../layouts/Loading';
+
 function Project() {
   const { id } = useParams();
   const [project, setProject] = useState([]);
 
   useEffect(() => {
-    fetch(`http://localhost:5000/projects/${id}`, {
+    setTimeout(() =>{
+      fetch(`http://localhost:5000/projects/${id}`, {
       method: 'GET',
       headers: {
         'Content-Type': 'application/json'
       },
     })
-    .then((resp) => resp.json())
-    .then((data) =>{
-      setProject(data)
-    })
-    .catch((err) => console.log(err))
+      .then((resp) => resp.json())
+      .then((data) => {
+        setProject(data)
+      })
+      .catch((err) => console.log(err))
+    }, 500)
   }, [id])
 
-  return (<p>{project.name}</p>)
+  return (
+    <>
+      {project.name ? (<p>{project.name}</p>) : (
+        <Loading />
+      )}
+    </>
+  )
 }
 
 export default Project;
